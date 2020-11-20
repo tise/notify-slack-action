ca80571b0286b078f6a2e661499a8d1511102b7f
Alexander Weiss <ik@alexanderweiss.nl>
2020-11-20 17:01:22 +0100

    Initial version

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,68 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {IncomingWebhook} from '@slack/webhook'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const webhook = new IncomingWebhook(
+        core.getInput('webhook', {required: true})
+    )
+    const actor = core.getInput('actor') || process.env.GITHUB_ACTOR
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const subject = core.getInput('subject')
+    const event = core.getInput('event')
+    const mention = core.getInput('mention')
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+    const repoUrl = `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}`
+    const runUrl = `${repoUrl}/actions/runs/${process.env.GITHUB_RUN_ID}`
+    const commitUrl = `${repoUrl}/commit/${process.env.GITHUB_SHA}`
+
+    const title =
+        subject && event
+            ? [
+                  `<${runUrl}|${subject}>`,
+                  `${event}`,
+                  `on`,
+                  `\`<${commitUrl}|${process.env.GITHUB_REF}>\``,
+                  ...(mention ? [`<!${mention}>`] : [])
+              ].join(' ')
+            : core.getInput('title')
+    const message = core.getInput('message')
+    const footer = core.getInput('footer')
+
+    const text = title ? `*${title}*\n${message}` : message
+
+    await webhook.send({
+        username: core.getInput('username') || 'GitHub',
+        icon_emoji: core.getInput('icon_emoji') || undefined,
+        channel: core.getInput('channel') || undefined,
+        unfurl_links: false,
+        attachments: [
+            {
+                // fallback: '', TODO
+                color: core.getInput('color') || 'good',
+                author_name: actor,
+                author_link: `${process.env.GITHUB_SERVER_URL}/${actor}`,
+                author_icon: `${process.env.GITHUB_SERVER_URL}/${actor}.png?size=32`,
+                footer:
+                    footer || `<${repoUrl}:${process.env.GITHUB_REPOSITORY}>`,
+                footer_icon: footer
+                    ? undefined
+                    : 'https://github.githubassets.com/favicon.ico',
+                fields: [
+                    {
+                        title: undefined as any, // This isn't required according to API spec
+                        value: text,
+                        short: false
+                    }
+                ]
+            }
+        ]
+    })
 }
 
-run()
+;(async () => {
+    try {
+        run()
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+})()
