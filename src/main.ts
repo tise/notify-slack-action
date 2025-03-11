1bcced8653a44edaf3bd366c8a85070c3783c4aa
Prasad Ostwal <prasadostwal@gmail.com>
2025-03-11 13:22:11 +0100

    a

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function run(): Promise<void> {
     const event = core.getInput('event')
     const mention = core.getInput('mention')
     const threaded_message = core.getInput('threaded_message')
+    const text = core.getInput('text')
 
     const repoUrl = `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}`
     const runUrl = `${repoUrl}/actions/runs/${process.env.GITHUB_RUN_ID}`
@@ -30,7 +31,7 @@ async function run(): Promise<void> {
     const message = core.getInput('message')
     const footer = core.getInput('footer')
 
-    const text = [
+    const messageText = [
         ...(title ? [`*${title}*`] : []),
         ...(message ? [message] : [])
     ].join('\n')
@@ -39,29 +40,31 @@ async function run(): Promise<void> {
         username: core.getInput('username'),
         icon_emoji: core.getInput('icon_emoji') || undefined,
         channel: core.getInput('channel'),
-        unfurl_linkss: false,
-        text: '',
-        attachments: [
-            {
-                // fallback: '', TODO
-                color: core.getInput('color'),
-                author_name: actor,
-                author_link: `${process.env.GITHUB_SERVER_URL}/${actor}`,
-                author_icon: `${process.env.GITHUB_SERVER_URL}/${actor}.png?size=32`,
-                footer:
-                    footer || `<${repoUrl}|${process.env.GITHUB_REPOSITORY}>`,
-                footer_icon: footer
-                    ? undefined
-                    : 'https://github.githubassets.com/favicon.ico',
-                fields: [
-                    {
-                        title: undefined as any, // This isn't required according to API spec
-                        value: text,
-                        short: false
-                    }
-                ]
-            }
-        ]
+        unfurl_links: false,
+        text: inputText || '',
+        attachments: !inputText
+            ? [
+                  {
+                      color: core.getInput('color'),
+                      author_name: actor,
+                      author_link: `${process.env.GITHUB_SERVER_URL}/${actor}`,
+                      author_icon: `${process.env.GITHUB_SERVER_URL}/${actor}.png?size=32`,
+                      footer:
+                          footer ||
+                          `<${repoUrl}|${process.env.GITHUB_REPOSITORY}>`,
+                      footer_icon: footer
+                          ? undefined
+                          : 'https://github.githubassets.com/favicon.ico',
+                      fields: [
+                          {
+                              title: undefined as any,
+                              value: messageText,
+                              short: false
+                          }
+                      ]
+                  }
+              ]
+            : undefined
     })
 
     if (threaded_message && response.ts) {
